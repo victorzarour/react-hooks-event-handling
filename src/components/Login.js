eda59815bd5a6ba185b90896746dd25bab40a51d
Victor <vx_zz@hotmail.com>
2022-06-07 11:44:08 -0400

    commit tree 8c586cbe8802e0a37ff7ec9d0276d4e14f85d30e

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ function Login() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Enter username..." />
+      <input onChange = {() => console.log("hey girl")} type="text" name="username" placeholder="Enter username..." />
       <input type="password" name="password" placeholder="Enter password..." />
       <button>Login</button>
     </form>
